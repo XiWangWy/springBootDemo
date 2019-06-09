6e2f64eeb29a2627095e0f874664f6179a0671be
XiWangWy <413933747@qq.com>
2019-06-09 10:23:18 +0800

    提交一个泛型的使用方法

--- src/main/java/com/bless/java8/CollectTest.java
+++ src/main/java/com/bless/java8/CollectTest.java
@@ -1,29 +1,29 @@
 package com.bless.java8;
 
-import com.google.common.collect.Lists;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 /**
  * Created by wangxi on 2019/3/26.
  */
 public class CollectTest<T,R> {
     public static void main(String[] args) {
-        CollectTest<Integer,String> collectTest = new CollectTest<>();
+//        CollectTest<Integer,String> collectTest = new CollectTest<>();
+//
+//        Function<Integer,String> function = (intStr) ->  intStr.toString() +  "hh";
+//        String ss = collectTest.test(function,15);
+//        System.out.println(ss);
+          int a = 7;
+          System.out.println(a & 123);
 
-        Function<Integer,String> function = (intStr) ->  intStr.toString() +  "hh";
-        String ss = collectTest.test(function,15);
-        System.out.println(ss);
     }
 
     private R test(Function<T,R> function, T t){
         return function.apply(t);
+    }
+
+    private static void test(){
+        HashMap map = new HashMap();
+        map.put("aa","bb");
     }
 }
